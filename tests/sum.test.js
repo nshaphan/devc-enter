cdf3dbffaf6c92b1bd781675085dc217dbd0b842
Charles Odili <chaluwa@gmail.com>
2020-02-09 12:43:55 +0100

    correctly add numbers from a collection

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -10,5 +10,6 @@ test("adds more than 2 numbers", () => {
 
 test("adds more than 2 numbers correctly", () => {
   expect(sum(1, 2, 3, 4, 5, 6)).toBe(21);
-  expect(sum(1, 2, -1, 3, 4, 5, 1, undefined, 6)).toBe(21);
+  expect(sum(1, 2, -1, 3, 4, 5, 1, ' ', undefined, 6)).toBe(21);
 });
+
