e9e58bdadf23aa54444983456b58fa0ccb160cfa
Charles Odili <chaluwa@gmail.com>
2020-02-09 11:17:28 +0100

    add a collection of numbers

--- src/sum.js
+++ src/sum.js
@@ -1,6 +1,5 @@
 function sum(a, b, ...others) {
-  //const all = [a, b, ...others];
-  //return all.reduce((total, num) => total + num, 0);
-  return a + b;
+  const all = [a, b, ...others];
+  return all.reduce((total, num) => total + num, 0);
 }
 module.exports = sum;
