620982c3527cd54cb4af548ee11e0bc79ac1fd17
Charles Odili <chaluwa@gmail.com>
2020-02-09 13:12:32 +0100

    fail tests

--- src/sum.js
+++ src/sum.js
@@ -1,5 +1,5 @@
 function sum(a, b, ...others) {
-  const all = [a, b, ...others].filter(n => `${n}`.trim() !== '' && !isNaN(n));
+  const all = [a, b, ...others];//.filter(n => `${n}`.trim() !== '' && !isNaN(n));
   return all.reduce((total, num) => total + num, 0);
 }
 module.exports = sum;
