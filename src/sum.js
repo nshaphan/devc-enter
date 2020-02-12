96c0b44f225d6b1fa04ffb6847bdaeff56c04a5e
Charles Opute Odili <chaluwa@gmail.com>
2020-02-12 12:20:09 +0100

    Update sum.js

--- src/sum.js
+++ src/sum.js
@@ -1,5 +1,4 @@
-function sum(a, b, ...others) {
-  const all = [a, b, ...others].filter(n => `${n}`.trim() !== '' && !isNaN(n));
-  return all.reduce((total, num) => total + num, 0);
+function sum() {
+  
 }
 module.exports = sum;
