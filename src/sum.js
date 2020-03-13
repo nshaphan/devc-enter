c7742e062de11f496966ebea934daf9b9dafb6c9
Charles Odili <chaluwa@gmail.com>
2020-03-13 16:47:59 +0100

    prepare to externalise tests

--- src/sum.js
+++ src/sum.js
@@ -2,3 +2,8 @@ const sum = () => {
 };
 
 export default sum;
+
+// const nums = [a, b, ...others]
+//     .map((n) => parseInt(n, 10))
+//     .filter((n) => !Number.isNaN(n));
+//   return nums.reduce((total, n) => total + n, 0);
